d4688d25251d3768bd8ec4b5afb1742a477ab55e
Alexis <alex10o@hotmail.com>
2021-11-11 18:18:10 -0300

    un comentario sobreee  AXIOS

--- src/components/UsarAPI/UsarAPI.js
+++ src/components/UsarAPI/UsarAPI.js
@@ -6,7 +6,7 @@ const urlApi = 'http://localhost:8080/api/productos'
 class UsarAPI {
     getProductos(){
         
-        return axios.get(urlApi); //no olvidase de intallar axios 
+        return axios.get(urlApi); //no olvidase de intallar axios --> npm i axios
         //console.log(urlApi);
         
     }
